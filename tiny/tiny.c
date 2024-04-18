41e0c177b62839b0a0f0d81beece2367ee526872
hyeong1 <mi75625@gmail.com>
2024-04-18 21:51:11 +0900

    feat: read_requesthdrs

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -84,6 +84,18 @@ void doit(int fd)
   }
 }
 
+void read_requesthdrs(rio_t *rp)
+{
+  char buf[MAXLINE];
+
+  Rio_readlineb(rp, buf, MAXLINE);
+  while (strcmp(buf, "\r\n")) {
+    Rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+  }
+  return;
+}
+
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
 {
   char buf[MAXLINE], body[MAXBUF];
